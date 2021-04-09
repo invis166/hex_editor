c753a753de96ffef13aa5a511de4d7ee845b5a51
skypunkaudiowalker <kolesnikovv028@gmail.com>
2021-04-09 14:07:01 +0500

    add HexEditor class

---
+++ hex_editor.py
@@ -0,0 +1,14 @@
+class HexEditor:
+	def __init__(self, filename):
+		self.fp = open(filename, 'r+b')
+
+	def get_hex_view(self):
+		print(self.fp.read())
+
+	def __del__(self):
+		self.fp.close()
+
+
+
+if __name__ == '__main__':
+	pass
