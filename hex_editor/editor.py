121de92afbc1a982f50551c1a114b88661c1c2ae
skypunkaudiowalker <kolesnikovv028@gmail.com>
2021-04-09 20:58:24 +0500

    add read_nbytes

--- hex_editor/editor.py
+++ hex_editor/editor.py
@@ -1,9 +1,22 @@
 class HexEditor:
-	def __init__(self, filename):
+	def __init__(self, filename: str):
 		self.fp = open(filename, 'r+b')
+		self.chunk_size = 1024
+
+	def read_nbytes(self, count: int, offset: int=None):
+		if offset != None:
+			self.fp.seek(offset)
+
+		while data := self.fp.read(chunk := min(self.chunk_size, count)):
+			yield from data
+			count -= chunk
 
 	def get_hex_view(self):
-		return self.fp.read().hex(' ')
+		return ' '.join(map(lambda x: hex(x)[2:], 
+			            self.read_nbytes(self.chunk_size)))
+
+	def change(self, start: int, stop: int, data: bytes):
+		pass
 
 	def __del__(self):
 		self.fp.close()
