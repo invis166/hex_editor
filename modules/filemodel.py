961d57ba909b6283aeb4425f5893cfcef789fca4
skypunkaudiowalker <kolesnikovv028@gmail.com>
2021-04-21 12:53:29 +0500

    rename fileregion to filemodel and add FileModel class

--- modules/filemodel.py
+++ modules/filemodel.py
@@ -1,5 +1,22 @@
 from functools import total_ordering
+import bisect
 
+
+class FileModel:
+	def __init__(self):
+		pass
+
+	def search_regions(self, count: int, offset: int):
+		region_index = bisect.bisect_left(self._regions, offset)
+
+		result = []
+		if region_index != -1:
+			while self._regions[region_index] <= offset + count:
+				print('here')
+				result.append(self._regions[region_index])
+				region_index += 1
+
+		return result
 
 @total_ordering
 class FileRegion:
