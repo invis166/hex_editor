fe90f05680036cf0b4d21402da603af8cf770f90
skypunkaudiowalker <kolesnikovv028@gmail.com>
2021-04-22 13:24:55 +0500

    add EditedFileRegion class

--- modules/filemodel.py
+++ modules/filemodel.py
@@ -4,11 +4,10 @@ from functools import total_ordering
 
 @total_ordering
 class FileRegion:
-    def __init__(self, start, end, index, is_inserted=False):
+    def __init__(self, start, end, index):
         self.start = start
         self.end = end
         self.index = index
-        self.is_inserted = is_inserted
 
     def __eq__(self, other):
         if isinstance(other, int):
@@ -26,9 +25,18 @@ class FileRegion:
         return f'FileRegion({self.start}, {self.end})'
 
 
+class EditedFileRegion(FileRegion):
+    def __init__(self, start, index, data):
+        super().__init__(start, len(data) + start, index)
+        self.data = data
+
+    def get_nbytes(self, count):
+        return self.data[:count]
+
+
 class FileModel:
     def __init__(self, file_size: int):
-        self.size = file_size
+        self.size: int = file_size
         self.file_regions = [FileRegion(0, self.size - 1, 0)]
 
     def search_region(self, offset: int) -> FileRegion:
