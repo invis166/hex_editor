1ffeb7aff4f484213d55a8c6b9b42c52caf2d134
skypunkaudiowalker <kolesnikovv028@gmail.com>
2021-04-25 02:33:53 +0500

    some refactoring

--- test/filemodel_tests.py
+++ test/filemodel_tests.py
@@ -1,13 +1,16 @@
 import unittest
 
-from modules.filemodel import FileRegion, FileModel
+from modules.filemodel import FileRegion, EditedFileRegion, FileModel
 
 
-def _convert_tuples_to_regions(*regions):
-    return [FileRegion(*region) for region in regions]
+def convert_tuples_to_regions(*regions, is_edited=False):
+    if is_edited:
+        return [EditedFileRegion(region[0], list(range(region[1])), index)
+                for index, region in enumerate(regions)]
+    return [FileRegion(*region, index) for index, region in enumerate(regions)]
 
 
-def _convert_regions_to_tuples(regions):
+def convert_regions_to_tuples(regions):
     return [(region.start, region.end) for region in regions]
 
 
@@ -16,59 +19,59 @@ class FileModelTestCase(unittest.TestCase):
         self.model = FileModel(0)
 
     def test_search(self):
-        self.model.file_regions = _convert_tuples_to_regions(
+        self.model.file_regions = convert_tuples_to_regions(
             (0, 5), (6, 10), (11, 17), (18, 30))
 
-        self._test_search([(0, 5)], 1, 0)
-        self._test_search([(0, 5)], 2, 0)
-        self._test_search([(0, 5)], 3, 0)
-        self._test_search([(0, 5)], 4, 0)
-        self._test_search([(0, 5)], 5, 0)
+        self._test_search([(0, 5)], 1)
+        self._test_search([(0, 5)], 2)
+        self._test_search([(0, 5)], 3)
+        self._test_search([(0, 5)], 4)
+        self._test_search([(0, 5)], 5)
 
-        self._test_search([(11, 17)], 11, 0)
-        self._test_search([(11, 17)], 12, 0)
-        self._test_search([(11, 17)], 13, 0)
-        self._test_search([(11, 17)], 14, 0)
-        self._test_search([(11, 17)], 15, 0)
+        self._test_search([(11, 17)], 11)
+        self._test_search([(11, 17)], 12)
+        self._test_search([(11, 17)], 13)
+        self._test_search([(11, 17)], 14)
+        self._test_search([(11, 17)], 15)
 
-        self._test_search([(0, 5)], 0, 3)
-        self._test_search([(0, 5)], 0, 4)
-        self._test_search([(0, 5)], 0, 5)
+        self._test_search([(0, 5)], 0)
+        self._test_search([(0, 5)], 0)
+        self._test_search([(0, 5)], 0)
 
-        self._test_search([(0, 5), (6, 10)], 0, 6)
-        self._test_search([(0, 5), (6, 10)], 4, 6)
-        self._test_search([(6, 10), (11, 17), (18, 30)], 10, 20)
-        self._test_search([(0, 5), (6, 10), (11, 17), (18, 30)], 0, 30)
+        self._test_search([(0, 5), (6, 10)], 0)
+        self._test_search([(0, 5), (6, 10)], 4)
+        self._test_search([(6, 10), (11, 17), (18, 30)], 10)
+        self._test_search([(0, 5), (6, 10), (11, 17), (18, 30)], 0)
 
-    def _test_search(self, expected, offset, count):
-        result = _convert_regions_to_tuples(
-            self.model.search_regions(offset, count))
+    def _test_search(self, expected, offset):
+        result = convert_regions_to_tuples(
+            self.model.search_region(offset))
         self.assertSequenceEqual(expected, result)
 
 
 class FileRegionTestCase(unittest.TestCase):
     def test_eq(self):
-        self.assertTrue(FileRegion(1, 10) == 1)
-        self.assertTrue(FileRegion(1, 10) == 5)
-        self.assertTrue(FileRegion(1, 10) == 10)
+        self.assertTrue(FileRegion(1, 10, 0) == 1)
+        self.assertTrue(FileRegion(1, 10, 0) == 5)
+        self.assertTrue(FileRegion(1, 10, 0) == 10)
 
-        self.assertTrue(FileRegion(5, 10) != 4)
-        self.assertTrue(FileRegion(5, 10) != 11)
+        self.assertTrue(FileRegion(5, 10, 0) != 4)
+        self.assertTrue(FileRegion(5, 10, 0) != 11)
 
     def test_gt(self):
-        self.assertTrue(FileRegion(8, 10) > 7)
+        self.assertTrue(FileRegion(8, 10, 0) > 7)
 
-        self.assertFalse(FileRegion(8, 10) > 8)
-        self.assertFalse(FileRegion(8, 10) > 9)
-        self.assertFalse(FileRegion(8, 10) > 10)
+        self.assertFalse(FileRegion(8, 10, 0) > 8)
+        self.assertFalse(FileRegion(8, 10, 0) > 9)
+        self.assertFalse(FileRegion(8, 10, 0) > 10)
 
     def test_lt(self):
-        self.assertTrue(FileRegion(5, 10) < 11)
+        self.assertTrue(FileRegion(5, 10, 0) < 11)
 
-        self.assertFalse(FileRegion(5, 10) < 10)
-        self.assertFalse(FileRegion(5, 10) < 5)
-        self.assertFalse(FileRegion(5, 10) < 4)
-        self.assertFalse(FileRegion(5, 10) < 7)
+        self.assertFalse(FileRegion(5, 10, 0) < 10)
+        self.assertFalse(FileRegion(5, 10, 0) < 5)
+        self.assertFalse(FileRegion(5, 10, 0) < 4)
+        self.assertFalse(FileRegion(5, 10, 0) < 7)
 
 
 if __name__ == '__main__':
