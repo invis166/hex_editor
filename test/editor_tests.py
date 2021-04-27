dc2d98db2b58890507aa81590a06434ca612470c
skypunkaudiowalker <kolesnikovv028@gmail.com>
2021-04-28 00:01:03 +0500

    another simple test

--- test/editor_tests.py
+++ test/editor_tests.py
@@ -4,8 +4,14 @@ from modules.editor import HexEditor
 
 
 class HexEditorOnRealFileTestCase(unittest.TestCase):
-    def test(self):
+    def test1(self):
         editor = HexEditor('simple_file.txt')
         editor.insert_bytes(5, b'test')
         editor.insert_bytes(0, b'test')
         editor.replace_bytes(3, b'rep')
+
+    def test2(self):
+        editor = HexEditor('simple_file.txt')
+        editor.insert_bytes(7, b'dfg')
+        editor.replace_bytes(3, b'abc')
+        editor.insert_bytes(4, b'12345')
