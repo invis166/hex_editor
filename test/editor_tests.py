d8189030d8377025780a8fe49d88f73c73d5d8a0
skypunkaudiowalker <kolesnikovv028@gmail.com>
2021-04-28 12:34:12 +0500

    some tests

--- test/editor_tests.py
+++ test/editor_tests.py
@@ -6,12 +6,12 @@ from modules.editor import HexEditor
 class HexEditorOnRealFileTestCase(unittest.TestCase):
     def test1(self):
         editor = HexEditor('simple_file.txt')
-        editor.insert_bytes(5, b'test')
-        editor.insert_bytes(0, b'test')
-        editor.replace_bytes(3, b'rep')
+        editor.insert(5, b'test')
+        editor.insert(0, b'test')
+        editor.replace(3, b'rep')
 
     def test2(self):
         editor = HexEditor('simple_file.txt')
-        editor.insert_bytes(7, b'dfg')
-        editor.replace_bytes(3, b'abc')
-        editor.insert_bytes(4, b'12345')
+        editor.insert(7, b'dfg')
+        editor.replace(3, b'abc')
+        editor.insert(4, b'12345')
